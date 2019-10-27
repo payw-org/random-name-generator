dabba392ed47870da0a489e5b4a0565025411bcb
hchangjae <hcj@payw.org>
2019-10-27 15:51:50 +0900

    init: hello world

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req,res)=>{
+    res.send('hello world')
+    console.log('>requested')
+})
+
+
+
+app.listen(3000, ()=>{
+    console.log('now listening 3000 port')
+})

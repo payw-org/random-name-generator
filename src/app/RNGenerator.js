e5a5aa20b92146439ce9a2b3197db7785a408873
hchangjae <hcj@payw.org>
2019-10-28 14:40:22 +0900

    test: get random name

--- src/app/RNGenerator.js
+++ src/app/RNGenerator.js
@@ -4,18 +4,72 @@ class RNGenerator{
 
     static DBConnection
 
+    static getNumOfNoun(){
+        if(!this.DBConnection){
+            this.connectDB()
+        }
+
+        this.DBConnection.query('SELECT COUNT(*) FROM noun AS count', (err, result, filed)=>{
+            if(err){
+                console.error(err)
+                return -1
+            }
+
+            return result[0].count
+        })
+    }
+
+    static getNumOfAdjective(){
+        if(!this.DBConnection){
+            this.connectDB()
+        }
+
+        this.DBConnection.query('SELECT COUNT(*) FROM adjective AS count', (err, result, filed)=>{
+            if(err){
+                console.error(err)
+                return -1
+            }
+
+            return result[0].count
+        })
+    }
+
     static getRandomName(){
         if(!this.DBConnection){
             this.connectDB()
         }
 
-        this.DBConnection.query('SELECT COUNT(*) FROM noun', (err, result, filed)=>{
+        var numOfNoun = this.getNumOfNoun()
+        var numOfAdjective = this.getNumOfAdjective()
+
+        if(numOfNoun == -1 || numOfAdjective == -1){
+            return false
+        }
+
+        var idOfNoun = Math.floor(Math.random()*numOfNoun)
+        var idOfAdjective = Math.floor(Math.random()*numOfAdjective)
+
+        var noun , adjective
+        
+        this.DBConnection.query(`SELECT noun FROM noun WHERE id = ${idOfNoun}`, (err, result, filed) =>{
             if(err){
-                throw err
+                console.error(err)
+                return false
             }
 
-            console.log(result)
+            noun = result[0].noun
         })
+
+        this.DBConnection.query(`SELECT adjective FROM adjective WHERE id = ${idOfAdjective}`, (err, result, filed) =>{
+            if(err){
+                console.error(err)
+                return false
+            }
+
+            adjective = result[0].adjective
+        })
+
+        return adjective+noun
     }
 
     static connectDB(){

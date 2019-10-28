cc739452235e757205330ab73e79c12ca3c848cf
hchangjae <hcj@payw.org>
2019-10-28 12:01:51 +0900

    fix: simplify db.connect.config

--- src/config/db.connect.config.js
+++ src/config/db.connect.config.js
@@ -1,11 +1,9 @@
 require('dotenv').config()
 
 const env = {
-    mysql: {
-        user    : process.env.MYSQL_ID,
-        password: process.env.MYSQL_PW,
-        database: process.env.MYSQL_NAME
-    }
+    user    : process.env.MYSQL_ID,
+    password: process.env.MYSQL_PW,
+    database: process.env.MYSQL_NAME
 }
 
 module.exports = env

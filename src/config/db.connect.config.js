b2c88741a01e48b4b196ca6c6c993979a60b066c
hchangjae <hcj@payw.org>
2019-10-29 16:15:44 +0900

    test: rng endpoint

--- src/config/db.connect.config.js
+++ src/config/db.connect.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const env = {
-    host    : 'quizlunch.com',
+    host    : 'localhost',
     user    : process.env.MYSQL_ID,
     password: process.env.MYSQL_PW,
     database: process.env.MYSQL_NAME,

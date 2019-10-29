1d2debb4ecc46f1486342b76e04e2fd1c774016d
hchangjae <hcj@payw.org>
2019-10-29 16:58:22 +0900

    add: draft endpoint of manage

--- src/app.js
+++ src/app.js
@@ -2,10 +2,18 @@ const express = require('express')
 const RNGenerator = require('./app/RNGenerator')
 const app = express()
 
+const manage = require('./router/manage/index')//
+
+app.use('/manage',manage)
+
 app.get('/new', async (req,res)=>{
     const random_name = await RNGenerator.getRandomName()
     res.send(random_name)
     console.log(`${req.originalUrl} is requested`)
+})
+
+app.get('*',(req, res)=>{
+    res.send('X')
 })
 
 
---
+++ src/router/manage/noun.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/all',(req, res)=>{
+    res.send(req.originalUrl)
+})
+
+router.post('/add/.*',(req, res)=>{
+    res.send(req.originalUrl)
+})
+
+router.get('/sub/.*',(req, res)=>{
+    res.send(req.originalUrl)
+})
+
+module.exports = router

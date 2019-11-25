ca812414fd45ba9a2f7ce9fc8b8e24e61e6d1987
hchangjae <hcj@payw.org>
2019-11-25 16:46:11 +0900

    chore: indentation

--- src/app.js
+++ src/app.js
@@ -2,22 +2,22 @@ const express = require('express')
 const RNGenerator = require('./app/RNGenerator')
 const app = express()
 
-const manage = require('./router/manage/index')//
+const manage = require('./router/manage/index')
 
-app.use('/manage',manage)
+app.use('/manage', manage)
 
-app.get('/new', async (req,res)=>{
+app.get('/new', async (req, res) => {
     const random_name = await RNGenerator.getRandomName()
     res.send(random_name)
     console.log(`${req.originalUrl} is requested`)
 })
 
-app.get('*',(req, res)=>{
+app.get('*', (req, res) => {
     res.send('X')
 })
 
 
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('now listening 3000 port')
 })

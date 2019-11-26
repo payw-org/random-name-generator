f95084ba0936eb82052bc631b9747640c283ebb7
hchangjae <hcj@payw.org>
2019-11-26 20:52:06 +0900

    add: body-parser

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 const router = require('./router')
+const bodyParser = require('body-parser')
 const CORS = require('cors')();
 const port = 3100
 
 app.use(CORS);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/', router)
 
 app.listen(port, () => {

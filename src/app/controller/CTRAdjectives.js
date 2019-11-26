0ceda1f407c933beba4fe9bf4dfa04edccc504b4
hchangjae <hcj@payw.org>
2019-11-26 16:17:25 +0900

    refactor: more well structured Controller

--- src/app/controller/CTRAdjectives.js
+++ src/app/controller/CTRAdjectives.js
@@ -1,45 +1,27 @@
-const DBConnection = require('../DBConnector').getConnection()
+const DBAdjectives = require('../../db/DBAdjectives')
 
 async function selectAll(req, res) {
-    try {
-        var [result] = await DBConnection.query(`SELECT * FROM adjectives`)
-        return result
-    } catch (e) {
-        console.log(`>Error - ${e}`)
-    }
+    var result = await DBAdjectives.selectAll()
+    res.send(result)
 }
 
 async function insertByName(req, res) {
-    try {
-        var name = req.body.name
-        var [result] = await DBConnection.query(`SELECT _id FROM adjectives WHERE name = '${name}'`)
-
-        if (result.length === 0) {
-            await DBConnection.query(`INSERT INTO adjectives (name) VALUES (${name})`)
-            res.sendStatus(200)
-        }
-        else {// duplicate exist
-            res.send(`${name} is already exist`)
-        }
-    } catch (e) {
-        console.log(`>Error - ${e}`)
+    var result = await DBAdjectives.insertByName(req.body.name)
+    if(result){
+        res.sendStatus(200)
+    }
+    else{
+        res.send(`${req.body.name} is already exist`)        
     }
 }
 
 async function deleteByName(req, res) {
-    try {
-        var name = req.body.name
-        var [result] = await DBConnection.query(`SELECT _id FROM adjectives WHERE name = '${name}'`)
-
-        if (result.length !== 0) {
-            await DBConnection.query(`DELETE FROM adjectives WHERE _id = ${result[0]._id}`)
-            res.sendStatus(200)
-        }
-        else { // not exist
-            res.send(`${name} is not exist`)
-        }
-    } catch (e) {
-        console.log(`>Error - ${e}`)
+    var result = await DBAdjectives.deleteByName(req.body.name)
+    if(result){
+        res.sendStatus(200)
+    }
+    else{
+        res.send(`${req.body.name} is not exist`)        
     }
 }
 
--- src/app/controller/CTRNouns.js
+++ src/app/controller/CTRNouns.js
@@ -1,45 +1,27 @@
-const DBConnection = require('../DBConnector').getConnection()
+const DBNouns = require('../../db/DBNouns')
 
 async function selectAll(req, res) {
-    try {
-        var [result] = await DBConnection.query(`SELECT * FROM nouns`)
-        return result
-    } catch (e) {
-        console.log(`>Error - ${e}`)
-    }
+    var result = await DBNouns.selectAll()
+    res.send(result)
 }
 
 async function insertByName(req, res) {
-    try {
-        var name = req.body.name
-        var [result] = await DBConnection.query(`SELECT _id FROM nouns WHERE name = '${name}'`)
-
-        if (result.length === 0) {
-            await DBConnection.query(`INSERT INTO nouns (name) VALUES (${name})`)
-            res.sendStatus(200)
-        }
-        else {// duplicate exist
-            res.send(`${name} is already exist`)
-        }
-    } catch (e) {
-        console.log(`>Error - ${e}`)
+    var result = await DBNouns.insertByName(req.body.name)
+    if(result){
+        res.sendStatus(200)
+    }
+    else{
+        res.send(`${req.body.name} is already exist`)        
     }
 }
 
 async function deleteByName(req, res) {
-    try {
-        var name = req.body.name
-        var [result] = await DBConnection.query(`SELECT _id FROM nouns WHERE name = '${name}'`)
-
-        if (result.length !== 0) {
-            await DBConnection.query(`DELETE FROM nouns WHERE _id = ${result[0]._id}`)
-            res.sendStatus(200)
-        }
-        else { // not exist
-            res.send(`${name} is not exist`)
-        }
-    } catch (e) {
-        console.log(`>Error - ${e}`)
+    var result = await DBNouns.deleteByName(req.body.name)
+    if(result){
+        res.sendStatus(200)
+    }
+    else{
+        res.send(`${req.body.name} is not exist`)        
     }
 }
 

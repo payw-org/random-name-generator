3fe2858cb8453c19d0043a8158fe04100d5abc2a
hchangjae <hcj@payw.org>
2019-11-26 16:35:42 +0900

    add: CORS module

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const RNGenerator = require('./app/RNGenerator')
 const app = express()
 const router = require('./router')
+const CORS = require('cors')();
 const port = 3100
+
+app.use(CORS);
 app.use('/', router)
 
 app.listen(port, () => {

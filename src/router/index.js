d206b13b35c9408628f4b601bb24b808c67bd0b3
hchangjae <hcj@payw.org>
2019-11-26 16:29:05 +0900

    add: router index

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+
+const manage = require('./manage')
+
+
+router.use('/manage', manage)
+router.get('/new', async (req, res) => {
+    const random_name = await RNGenerator.getRandomName()
+    res.send(random_name)
+    console.log(`${req.originalUrl} is requested`)
+})
+router.get('*', (req, res) => {
+    res.send('Invalid request')
+})
+
+module.exports = router

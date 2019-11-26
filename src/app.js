e244836b9af206f3b868c93f36a475883da0792f
hchangjae <hcj@payw.org>
2019-11-26 16:28:49 +0900

    refactor: more simplify

--- src/app.js
+++ src/app.js
@@ -1,23 +1,10 @@
 const express = require('express')
 const RNGenerator = require('./app/RNGenerator')
 const app = express()
+const router = require('./router')
+const port = 3100
+app.use('/', router)
 
-const manage = require('./router/manage/index')
-
-app.use('/manage', manage)
-
-app.get('/new', async (req, res) => {
-    const random_name = await RNGenerator.getRandomName()
-    res.send(random_name)
-    console.log(`${req.originalUrl} is requested`)
-})
-
-app.get('*', (req, res) => {
-    res.send('X')
-})
-
-
-
-app.listen(3100, () => {
-    console.log('now listening 3100 port')
+app.listen(port, () => {
+    console.log(`now listening ${port} port`)
 })

d236b822a328f2fced4f47ffedf78b1419eeb707
hchangjae <hcj@payw.org>
2019-11-26 16:29:41 +0900

    chore: indentation & error point

--- src/router/manage/adjectives.js
+++ src/router/manage/adjectives.js
@@ -3,9 +3,10 @@ const CTRAdjectives = require('../../app/controller/CTRAdjectives')
 const router = express.Router()
 
 router.get('/all', CTRAdjectives.selectAll)
-
 router.post('/insert', CTRAdjectives.insertByName)
-
 router.delete('/delete', CTRAdjectives.deleteByName)
+router.get('*', (req, res) => {
+    res.send('Invalid request on manage/adjectives')
+})
 
 module.exports = router
--- src/router/manage/nouns.js
+++ src/router/manage/nouns.js
@@ -3,9 +3,10 @@ const CTRNouns = require('../../app/controller/CTRNouns')
 const router = express.Router()
 
 router.get('/all', CTRNouns.selectAll)
-
 router.post('/insert', CTRNouns.insertByName)
-
 router.delete('/delete', CTRNouns.deleteByName)
+router.get('*', (req, res) => {
+    res.send('Invalid request on manage/nouns')
+})
 
 module.exports = router
